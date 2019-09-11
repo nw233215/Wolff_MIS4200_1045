d83f925a3b6f268cd1aaa7e0bbae7151c78d4864
Norman Wolff <nw233215@ohio.edu>
2019-09-11 11:39:56 -0400

    Initial Model work

---
+++ Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wolff_MIS4200_1045.Models
+{
+    public class Customer
+    {
+        public int customerId { get; set; }
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public ICollection<order> orders { get; set; }
+    }
+}
---
+++ Models/order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wolff_MIS4200_1045.Models
+{
+    public class order
+    {
+        public int orderId { get; set; }
+        public int customerId { get; set; }
+        public virtual Customer customer { get; set; }
+        public DateTime orderDate { get; set; }
+    }
+}
